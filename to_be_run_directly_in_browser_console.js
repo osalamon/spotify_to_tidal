d2410c279d6c81011681139a22f6fe406a36d036
osalamon <ondrej.salamon@vsb.cz>
2026-01-10 23:42:24 +0100

    add JSON dump of my spotify extended stream history + git ignore it

--- to_be_run_directly_in_browser_console.js
+++ to_be_run_directly_in_browser_console.js
@@ -1,5 +1,5 @@
 // --- CONFIGURATION ---
-const TOKEN = "Bearer BQCPLHz5t-Sogils15QH6h2HPeoZq_KoOzuwhHtel4HxVwACuK62SHo9HfYAd4djj_m8P7T-YroWlLLpcsR2o4qRmZndqlxtWOUwCbTT6vYeES-k9SfzgHYF2xLv7wzQ_Z2s8cROq9cT5gMQgIi3qw1hlOOrXOpB60PiJaMq1lW8BF1YdacF7TbqHDqxBrf2idC119HAMO1sf2vqtsL6nRQHNpM4XS2aNgQ7mOLYsPJd_s-bCQwxtmOvCwBAJM3R_EARNQhErF8z8t4qvlVwGzcV8299aECJ8srSHmC9jgYx21EIaoxZ6QjO4mtrYLvDwyg68UL5wts8lttihoKlAaqtzlApc4XRf-4-Yh98AhUQhnGuguh5WpYof5LGiy_If6Nk9HVHAHge"; // <--- PASTE NEW TOKEN HERE
+const TOKEN = "Bearer BQC..."; // <--- PASTE NEW TOKEN HERE
 const DELAY_MS = 2500; // Keep it slow to avoid the '429' hammer
 
 // --- THE ENGINE ---
